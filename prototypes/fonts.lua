b99efe81983d527594b729b25aa88740f4e72e27
Guy Fraser <guy.fraser1@gmail.com>
2016-09-06 03:48:06 +0100

    Initial release v0.0.1

---
+++ prototypes/fonts.lua
@@ -0,0 +1,28 @@
+-- luacheck: globals data
+
+data:extend{
+  {
+    type = "font",
+    name = "glyph",
+    from = "glyph",
+    size = 14
+  },
+  {
+    type = "font",
+    name = "glyph-small",
+    from = "glyph",
+    size = 12
+  },
+  {
+    type = "font",
+    name = "glyph-medium",
+    from = "glyph",
+    size = 16
+  },
+  {
+    type = "font",
+    name = "glyph-large",
+    from = "glyph",
+    size = 18
+  }
+}
